0391de51cc5a995dca367a170b714261d5d1d820
hardipsolanki22 <hardipsolanki2224@gmail.com>
2026-03-07 09:07:47 +0530

    fix it

--- Frontend/src/components/order/SingleOrder.tsx
+++ Frontend/src/components/order/SingleOrder.tsx
@@ -149,7 +149,7 @@ const SingleOrder: React.FC = () => {
                     )}
                 </div>
                 {singleOrder.status === "DELIVERED" &&
-                    <div className='flex mt-3 sm:mt-0 sm:items-end w-full items-start sm:justify-end'>
+                    <div className='flex mt-3 sm:mt-0 sm:items-end w-full items-center sm:justify-end'>
                         <Button
                             onClick={() => navigate("/rating")}
                             className='cursor-pointer'>
