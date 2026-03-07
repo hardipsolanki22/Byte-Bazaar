a2e31f5f6193456b095a137868a311f83efa9403
hardipsolanki22 <hardipsolanki2224@gmail.com>
2026-03-07 13:20:45 +0530

    try to deploy backend to vercel

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,15 +4,20 @@ import { app } from './app.js'
 
 dotenv.config();
 
+// connectDB()
+//     .then(() => {
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Application is running on PORT ${process.env.PORT}`);
+//         });
+//         app.on('error', (error) => {
+//             console.log(`Error: ${error.message}`);
+//         })
+//     })
+//     .catch((error) => {
+//         console.log(`Error While Connected MongoDB...!: ${error.message}`);
+//     });
+
 connectDB()
-    .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log(`Application is running on PORT ${process.env.PORT}`);
-        });
-        app.on('error', (error) => {
-            console.log(`Error: ${error.message}`);
-        })
-    })
     .catch((error) => {
         console.log(`Error While Connected MongoDB...!: ${error.message}`);
     });
